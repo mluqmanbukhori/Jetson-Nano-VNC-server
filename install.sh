d1efb40a4c9a1fc9d5c6c0fef443122604d3dfc0
Muhammad Luqman Bukhori <44484883+mluqmanbukhori@users.noreply.github.com>
2022-03-19 01:53:26 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 set -e
-echo "Enable VNC server to start each time log in"
+
 sudo apt update
 sudo apt install -y vino
+
+# Enable the VNC server to start each time you log in
 
 # if you have a Jetson Nano 2GB Developer Kit (running LXDE)
 mkdir -p ~/.config/autostart
 cp /usr/share/applications/vino-server.desktop ~/.config/autostart/.
 
 # for all other Jetson developer (running GNOME) uncomment this
-cd /usr/lib/systemd/user/graphical-session.target.wants
-sudo ln -s ../vino-server.service ./.
+sudo ln -s ../vino-server.service \
+    /usr/lib/systemd/user/graphical-session.target.wants
 cd ~
 
 # Configure the VNC server
-gsetting set org.gnome.Vino prompt-enabled false
-gsetting set org.gnome.Vino require-encryption false
+gsettings set org.gnome.Vino prompt-enabled false
+gsettings set org.gnome.Vino require-encryption false
 
-echo "Set a password to access the VNC Server"
+# Set a password to access the VNC server
 # Replace [ jetson ] with your desired password
-gsetting set org.gnome.Vino authentication-methods "['vnc']"
-gsetting set org.gnome.Vino vnc-password $(echo -n 'jetson'|base64)
+gsettings set org.gnome.Vino authentication-methods "['vnc']"
+gsettings set org.gnome.Vino vnc-password $(echo -n 'jetson'|base64)
 
 echo "You've successfully activate VNC server on Jetson Nano"
-echo "Please Reboot system !"
+echo "Reboot the system so that the settings take effect"
 sudo reboot
